d2de5c224d7073f2e5e797e6547b417f0a51b3fa
System Administrator <root@macbooks-MacBook-Pro.local>
2022-01-06 22:58:09 +0700

    initial commit

---
+++ components/Results.js
@@ -0,0 +1,13 @@
+import Thumnail from "./Thumnail"
+import FlipMove from "react-flip-move"
+function Results({results}) {
+    return (
+        <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 "> 
+        {results.map ( result => 
+            <Thumnail key={result.id} result={result}/>
+            )}    
+        </FlipMove>
+    )
+}
+
+export default Results
